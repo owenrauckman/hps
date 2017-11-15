89963a18cfdf475addcc0976da9e1f6318bca034
owenrauckman <owenrauckman@gmail.com>
2017-11-14 20:02:16 -0600

    Builds Footer and adds to app

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
     <v-app class="m__app">
       <AppHeader/>
-      <router-view></router-view>
+      <router-view class="m__app__main"></router-view>
+      <AppFooter/>
     </v-app>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/Header/AppHeader';
+import AppFooter from '@/components/Footer/AppFooter';
 
 export default {
   name: 'app',
-  components: { AppHeader },
+  components: { AppHeader, AppFooter },
 };
 </script>
 
@@ -20,6 +22,12 @@ export default {
 @import './sass/main.scss';
 .m__app{
   background: $white !important; // vuetify override
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  &__main{
+    flex: 1 0 auto;
+  }
 }
 
 </style>
