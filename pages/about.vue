2f86ba9d6255a498596b8b8d4c1e1894c7524e5c
owenrauckman <owenrauckman@gmail.com>
2017-12-23 19:04:09 -0600

    Stands up dummy pages for about and pricing

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="m__create">
+    <h1 class="m__create__heading">About</h1>
+    <p class="m__create__description">We are making stuff...</p>
+  </div>
+</template>
---
+++ pages/pricing.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="m__create">
+    <h1 class="m__create__heading">Pricing</h1>
+    <p class="m__create__description">We were going to charge, but would rather help you with your business, so we are free :D</p>
+    <p class="m__create__description">Want to advertise? Contact us</p>
+  </div>
+</template>
